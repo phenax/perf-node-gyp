507117ff941cb9f2c3d9c82b63fdb055b6a39c5a
Akshay Nair <phenax5@gmail.com>
2016-12-17 20:34:27 +0530

    Refactoring

--- benchmark.js
+++ benchmark.js
@@ -3,9 +3,9 @@ const jsExample= require('./js_test.js');
 const cppExample= require('./cpp_test.js');
 
 
-function testFn(fn, input, name='function') {
+function testFn(func, args, name='function') {
 	console.time(name);
-	console.log(name, "output =", fn.apply(null, input).result);
+	console.log(name, "output =", func.apply(null, args).result);
 	console.timeEnd(name);
 }
 
@@ -15,7 +15,10 @@ console.log('\n----- RUNNING ------\n');
 let inputs= [ 10, 20, 30, 40, 45 ];
 
 inputs.forEach(inp => {
+	console.log('Input =', [ inp ]);
+
 	testFn(jsExample, [ inp ], 'Running with JS');
 	testFn(cppExample, [ inp ], 'Running with CPP');
+
 	console.log(' ');
 });
